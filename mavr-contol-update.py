7e384e2ff7a763f3343fc0b5ada5de013a3d5baa
Anatoly Beskakotov <beskakotov.as@gmail.com>
2018-02-13 14:04:24 +0300

    fix

--- mavr-contol-update.py
+++ mavr-contol-update.py
@@ -4,11 +4,11 @@ proxies = {
     "http" : "http://squid.sao.ru:8080",
     "https" : "http://squid.sao.ru:8080",
 }
-print(proxies)
 proxy_support = urllib.request.ProxyHandler(proxies)
 opener = urllib.request.build_opener(proxy_support)
 urllib.request.install_opener(opener)
 with urllib.request.urlopen('https://api.github.com/repos/Black13Wolf/linux_mavr_software/tags') as url:
     data = json.loads(url.read().decode())
-print(data)
-print(type(data))
+last_update = data[0]
+from pip import main as pip
+pip(['install', last_update['tarball_url'], '--proxy=http://squid.sao.ru:8080'])
